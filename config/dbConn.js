12a43c07e3f0169ede312b58c8bba02b5d988497
wingertjaylee <wingertjaylee@gmail.com>
2026-05-12 22:38:49 -0500

    Fix MongoDB connection options for Render

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI);
-        console.log('MongoDB Connected');
-        } catch (err) {
-            console.error('Error connecting to MongoDB:', err);
-        }
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+    }
 };
 
 module.exports = connectDB;
+
 
