326411153b73fb650dab8438b293ce1805b06e05
wingertjaylee <wingertjaylee@gmail.com>
2026-05-12 22:17:24 -0500

    Fix controller path case sensitivity for Render

--- routes/states.js
+++ routes/states.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const statesController = require('../Controllers/statesController');
+const statesController = require('../controllers/statesController');
 
 // GET routes
 router.get('/', statesController.getAllStates);
